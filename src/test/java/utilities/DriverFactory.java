36c217e34f86c9801b5013ee5256f61e52561a59
mateusz.styczynski <mw.styczynski@gmail.com>
2021-01-14 12:55:16 +0100

    Absolute path in DriverFactory physical webdriver file directory changed to project path directory

--- src/test/java/utilities/DriverFactory.java
+++ src/test/java/utilities/DriverFactory.java
@@ -7,17 +7,17 @@ public class DriverFactory {
     public static WebDriver setDriver(String browserType) {
         if (browserType.equalsIgnoreCase("chrome")){
             System.out.println("Testing " + browserType);
-            System.setProperty("webdriver.chrome.driver", "C:\\Users\\mateusz.styczynski\\Desktop\\untitled\\src\\main\\resources\\chromedriver.exe");
+            System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
             return new ChromeDriver();
         }
         else if (browserType.equalsIgnoreCase("firefox")){
             System.out.println("Testing " + browserType);
-            System.setProperty("webdriver.gecko.driver", "C:\\Users\\mateusz.styczynski\\Desktop\\untitled\\src\\main\\resources\\geckodriver.exe");
+            System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
             return new ChromeDriver();
         }
         else {
             System.out.println("Testing " + browserType);
-            System.setProperty("webdriver.chrome.driver", "C:\\Users\\mateusz.styczynski\\Desktop\\untitled\\src\\main\\resources\\chromedriver.exe");
+            System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
             return new ChromeDriver();
         }
 
